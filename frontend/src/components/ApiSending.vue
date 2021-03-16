63233a3e1d431557fa8bc036780635f170db379d
ebiryu <ryuta.ebihara@stockmark.co.jp>
2021-03-16 16:58:13 +0900

    add rollDie method

--- frontend/src/components/ApiSending.vue
+++ frontend/src/components/ApiSending.vue
@@ -18,13 +18,21 @@ export default defineComponent({
   },
   methods: {
     send() {
+      var dice = 3;
+      var sides = 6;
+      var query = `query RollDice($dice: Int!, $sides: Int) {
+  rollDice(numDice: $dice, numSides: $sides)
+}`;
       fetch(API_HOST + "/graphql", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
           Accept: "application/json",
         },
-        body: JSON.stringify({ query: "{ random }" }),
+        body: JSON.stringify({
+          query,
+          variables: { dice, sides },
+        }),
       })
         .then((r) => r.json())
         .then((data) => {
