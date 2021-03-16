dba51b1d304b474c9ef9a75f1127ca3dcee5268b
ebiryu <ryuta.ebihara@stockmark.co.jp>
2021-03-16 15:36:01 +0900

    add api request

--- frontend/src/components/ApiSending.vue
+++ frontend/src/components/ApiSending.vue
@@ -1,10 +1,37 @@
 <template>
-  <div><button>GraphQLへリクエストする</button></div>
+  <div>
+    <button @click="send">GraphQLへリクエストする</button>
+    <p>{{ responseValue }}</p>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+
+const API_HOST = "http://localhost:4000";
+const data = { responseValue: "" };
+
 export default defineComponent({
   name: "ApiSending",
+  data() {
+    return data;
+  },
+  methods: {
+    send() {
+      fetch(API_HOST + "/graphql", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({ query: "{ hello }" }),
+      })
+        .then((r) => r.json())
+        .then((data) => {
+          console.log("data returned:", data);
+          this.responseValue = JSON.stringify(data);
+        });
+    },
+  },
 });
 </script>
